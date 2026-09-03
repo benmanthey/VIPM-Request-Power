71c530b1f9ec77d7fb6d19ad36823bcbcf270ac4
Ben Manthey <ben.manthey@gmail.com>
2026-09-03 17:43:21 -0600

    bat file now accepts command line arguments. Prepping for pre-build LabVIEW file.

--- sbom/make sbom.bat
+++ sbom/make sbom.bat
@@ -1,11 +1,9 @@
-set /p "ver=Enter VIPB version: "
+:: command line looks like make sbom.bat 1.0.0.3
 
 vipm sbom "..\source\VIPM Request Power.lvproj" ^
 --format "cyclonedx" ^
 --schema-version "1.5" ^
 --labview-bitness "32" ^
 --product-name "Request Power" ^
---product-version %ver% ^
+--product-version %1 ^
 --output "..\sbom\sbom.json"
-
-pause
